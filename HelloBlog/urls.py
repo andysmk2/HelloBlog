94ba24e44a1bd4d29b5c82b3dc2dd2074c9cc41f
Ryan <tripper@TripperdeMacBook-Pro.local>
2016-05-27 01:56:37 +0800

    fix little bugs

--- HelloBlog/urls.py
+++ HelloBlog/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 
-import News
+import News.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', News.views.home),
