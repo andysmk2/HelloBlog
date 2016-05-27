4baa1f4f7e4528887f7e1dc9134d501eaa8a52e0
Ryan Chao <ryanchao2012@gmail.com>
2016-05-27 18:42:35 +0800

    add new models: Page, Article, etc. (#8)
    * init
    
    * add first page
    
    * stop tracking useless files
    
    * fix url setting conflict
    
    * fix little bugs
    
    * add ignore file
    
    * sdfasf
    
    * remove useless files
    
    * add models

--- News/admin.py
+++ News/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+import models
 # Register your models here.
+
+admin.site.register(models.Category)
+admin.site.register(models.Page)
+admin.site.register(models.Article)
+admin.site.register(models.Author)
+
--- News/models.py
+++ News/models.py
@@ -3,3 +3,30 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length = 20)
+    disc = models.CharField(max_length = 100, blank = True)
+
+
+class Page(models.Model):
+    title = models.CharField(max_length = 50)
+    subtitle = models.CharField(max_length = 50, blank = True)
+    category = models.ForeignKey(Category)
+
+class Article(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField(max_length = 500)
+    author = models.ForeignKey(Author)
+    category = models.ManyToManyField(Category)
+    # picture = models
+    date = models.DateTimeField()
+
+
+
